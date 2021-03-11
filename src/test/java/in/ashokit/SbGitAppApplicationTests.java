d50f4dc8533f304c8cd2855da795d86200776636
jyotirmayee132 <sahoojyotirmayee385@gmail.com>
2021-03-10 19:23:28 -0800

    InItial commit

---
+++ src/test/java/in/ashokit/SbGitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ashokit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbGitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
