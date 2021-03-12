aaed44227a40c5908e64b60b6a26d33786e1bdee
jyotirmayee132 <sahoojyotirmayee385@gmail.com>
2021-03-12 07:29:06 -0800

    HIS 300 changes

--- src/main/java/in/ashokit/SbGitAppApplication.java
+++ src/main/java/in/ashokit/SbGitAppApplication.java
@@ -6,12 +6,17 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SbGitAppApplication {
+     private Map<String,Object> cache=new HashMap<String,Object>();
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(SbGitAppApplication.class, args);
 		//HIS 134 changes
 		run.close();
               int a =12;
+   // HiS 301 changes
+     public void loadCache(){
+      // logic
+}
 	}
 
 }
